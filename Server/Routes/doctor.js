2ff6f77eabee44fdfdad59b5bc68598758b34dec
KanikaMS <32144555+KanikaMS@users.noreply.github.com>
2017-11-09 21:17:57 +0530

    Update doctor.js

--- Server/Routes/doctor.js
+++ Server/Routes/doctor.js
@@ -3,7 +3,7 @@ var Doctor     = require('../models/doctor');
 module.exports = function(doctorRouter){
 
 //delete doctor login credentials  http://localhost:port/api/doctor/del/:id
-doctorRouter.get('/del/:id', function(req,res, next){
+doctorRouter.get('/deleteDoctor/:id', function(req,res, next){
     Doctor.findOne({_id: mongojs.ObjectId(req.params.id)}).remove(function(err){
     if(err)	res.json(err);
     else { res.json({success: 'true'}); }
@@ -12,16 +12,16 @@ doctorRouter.get('/del/:id', function(req,res, next){
  
 }); 
 //add
-doctorRouter.put('/add/:id', function(req, res){
+doctorRouter.put('/addExperience/:id', function(req, res){
     var doctor = {
        
-        experience: [{
-            from: req.body.experience.from,
-            to: req.body.experience.to,
-            hospital: req.body.experience.hospital,
-            post: req.body.experience.post,
-            description: req.body.experience.description
-           } ]               
+        experience: [
+            {from: req.body.experience.from},
+            {to: req.body.experience.to},
+            {hospital: req.body.experience.hospital},
+            {post: req.body.experience.post},
+            {description: req.body.experience.description}
+            ]               
     };
    var opts = { strict: false };
    Doctor.update({_id: mongojs.ObjectId(req.params.id)}, doctor, opts, function(error) {
@@ -33,7 +33,7 @@ doctorRouter.put('/add/:id', function(req, res){
 
 //delete data in array
 
-doctorRouter.put('/remove/:id/:experienceid', function(req,res){
+doctorRouter.put('/removeDoctor/:id/:experienceid', function(req,res){
 
 var my_id = req.params.id,//assume get 54fcb3890cba9c4234f5c925
 experience_id = req.params.experienceid;// assume get 54fcb3890cba9c4234f5c925
